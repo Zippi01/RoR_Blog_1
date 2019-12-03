c839a07dd530fb9302fc218a1e83fae3b8846733
Zippi01 <katyakovtun080619@gmail.com>
2019-12-03 15:31:10 +0200

    install psql && fix migration

--- db/migrate/20191117144648_change_name_to_author_id.rb
+++
@@ -1,7 +0,0 @@
-class ChangeNameToAuthorId < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :posts, :name, :author_id
-
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_12_03_105601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -33,12 +36,20 @@ ActiveRecord::Schema.define(version: 2019_12_03_105601) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-# Could not dump table "authors" because of following StandardError
-#   Unknown type 'uniq' for column 'email'
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
+    t.string "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_digest"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
@@ -74,8 +85,8 @@ ActiveRecord::Schema.define(version: 2019_12_03_105601) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "comment_id", null: false
-    t.integer "author_id", null: false
+    t.bigint "comment_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "likes"
